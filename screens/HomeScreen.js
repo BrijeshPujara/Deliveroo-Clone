14cf074934a0f77f3a204ae0147d22b818878af0
pujara.brijesh1@gmail.com <pujara.brijesh1@gmail.com>
2022-12-30 14:22:27 +0000

    fix: tailwind config updated and styling working

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -13,19 +14,22 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView>
-      <Text className='text-red-500'>
-        {/* Header */}
-        <View>
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-              height: 30,
-              width: 40
-            }}
-            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
-          />
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "http://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs"> Deliver Now!</Text>
+          <Text className="font-bold text-xl"> Current Location <ChevronDownIcon size={20} color="#00CCBB"  /> </Text>
+          
         </View>
-      </Text>
+
+        <UserIcon size={35} color="#00CCBB"/>
+      </View>
     </SafeAreaView>
   );
 }
